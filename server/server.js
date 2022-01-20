8c5b70acda4a5c5d3587b9af82c028010a77f4a3
anwsimon <anna.w.simon1@gmail.com>
2022-01-19 19:31:51 -0500

    added file serving express route

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
 //express middleware
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const startup = async () => {
   await server.start()
   server.applyMiddleware({ app })
