263c714fce10c54be23fa1918f621d298cff84af
anwsimon <anna.w.simon1@gmail.com>
2022-01-19 22:10:34 -0500

    changed cors options

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const { ApolloServer } = require('apollo-server-express');
 const NasaAPI = require('../src/datasource');
 const typeDefs  = require('./schema')
 const resolvers = require('./resolver')
-const path = require('path')
+const cors = require("cors");
+
+var corsOptions = {
+  origin: '*',
+  credentials: true
+};
 
 const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
   return {
@@ -14,24 +19,16 @@ const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
 //express middleware
 const app = express();
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 const startup = async () => {
   await server.start()
-  server.applyMiddleware({ app })
+  server.applyMiddleware({ app, cors: false })
   return app
 }
-
+app.use(cors(corsOptions))
 
 startup()
+
 app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at https://studio.apollographql.com/dev`)
 );
-
-// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-//   console.log(`🚀 Server ready at ${url}`);
-// });
 
--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ import {
 
 
 const client = new ApolloClient({
-  uri: 'https://blooming-gorge-40706.herokuapp.com/graphql',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
-  opts: {
+  fetchOptions: {
     mode: "no-cors",
   }
 });
