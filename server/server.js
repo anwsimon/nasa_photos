b4cfd5ec70ec41dca29b4832bd3b6017e5cd31c8
anwsimon <anna.w.simon1@gmail.com>
2022-01-19 19:27:16 -0500

    changed server listen script

--- server/server.js
+++ server/server.js
@@ -14,16 +14,6 @@ const server = new ApolloServer({ typeDefs, resolvers, dataSources: () => {
 //express middleware
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 const startup = async () => {
   await server.start()
   server.applyMiddleware({ app })
@@ -32,11 +22,11 @@ const startup = async () => {
 
 
 startup()
-// app.listen({ port: process.env.PORT || 4000 }, () =>
-//   console.log(`🚀 Server ready at https://studio.apollographql.com/dev`)
-// );
+app.listen({ port: process.env.PORT || 4000 }, () =>
+  console.log(`🚀 Server ready at https://studio.apollographql.com/dev`)
+);
 
-app.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
 
