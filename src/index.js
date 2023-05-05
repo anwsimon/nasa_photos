b3eb4d1fb218c3ced338ac34643855facfdbbfab
anwsimon <anna.w.simon1@gmail.com>
2023-05-05 13:19:18 -0400

    updated graphql endpoint

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import App from "./App"
 
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-} from "@apollo/client";
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: 'https://cat-snaps-from-space.herokuapp.com/graphql',
+  uri: "https://main--distracted-lalande-1c0d5c.netlify.app/graphql",
   cache: new InMemoryCache(),
   fetchOptions: {
     mode: "no-cors",
-  }
-});
-
+  },
+})
 
 ReactDOM.render(
-  <ApolloProvider client = {client}>
-      <App />
+  <ApolloProvider client={client}>
+    <App />
   </ApolloProvider>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 export default client
